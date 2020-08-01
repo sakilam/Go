a34df924dce48f05215df87332b2d503e3fff22b
Sandeep Sakilam <sandeep.sakilam@atmecs.com>
2020-08-01 21:35:39 +0530

    Example code for define variables & print

---
+++ 1_define_and_print.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main() {
+  firstName := "Test"
+  familyName := "User"
+  age := 21
+  peanutAllergy := true
+  fmt.Println(firstName)
+  fmt.Println(familyName)
+  fmt.Println(age)
+  fmt.Println(peanutAllergy)
+}
+
